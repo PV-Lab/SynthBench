fa0dd1a4c78eaf5b4eb962054c42fe21a1e152e9
steventorrisi-tri <steven.torrisi@tri.global>
2025-04-22 11:56:53 -0700

    Renamed some stuff; used black

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='synthbench',
-    version='0.1.0',
-    description='A synthetic hypothesis testing environment for testing synthesizability filters.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Basita Das',
-    author_email='dasb@mit.edu',
-    url='https://github.com/PV-Lab/SynthBench',
+    name="synthbench",
+    version="0.1.0",
+    description="A synthetic hypothesis testing environment for testing synthesizability filters.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Basita Das",
+    author_email="dasb@mit.edu",
+    url="https://github.com/PV-Lab/SynthBench",
     packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=open("requirements.txt").read().splitlines(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11',
+    python_requires=">=3.11",
 )
